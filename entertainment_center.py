dc9995291398dcd18ce7f734cde6c1a3f3642e95
Jeff <jeffdavidthompson@gmail.com>
2015-06-08 14:55:31 -0700

    shorten all lines to under 79 chars

--- entertainment_center.py
+++ entertainment_center.py
@@ -20,13 +20,16 @@ shining = media.Movie("The Shining",
                       "After taking a job as a lodge caretaker the Torrences \
                       must come together and learn the true meaning of \
                       Christmas as they stay for the winter",
-                      "http://40.media.tumblr.com/2a5fb58e48e7ffc36e3afbe50f83d0fe/tumblr_nhzc3uHBcF1tk4n0bo4_1280.jpg",
+                      "http://40.media.tumblr.com/2a5fb58e48e7ffc36e3afbe50f8"
+                      "3d0fe/tumblr_nhzc3uHBcF1tk4n0bo4_1280.jpg",
                       "https://www.youtube.com/watch?v=rt15HCq4htw")
 
 mad_max = media.Movie("Mad Max: Fury Road",
                       "After our zaney hero gets a bad blood transfusion his \
                       vacation to palm spings goes haywire.",
-                      "http://cdn3-www.superherohype.com/assets/uploads/gallery/mad-max-fury-road_1/10636937_661847177254140_3001186770164503894_o.jpg",
+                      "http://cdn3-www.superherohype.com/assets/uploads/galler"
+                      "y/mad-max-fury-road_1/10636937_661847177254140_30011867"
+                      "70164503894_o.jpg",
                       "https://www.youtube.com/watch?v=hEJnMQG9ev8")
 
 it_follows = media.Movie("It Follows",
@@ -39,14 +42,16 @@ guardians = media.Movie("Guardians of the Galaxy",
                         "A group of friends search the stars for excitement \
                         after one racoon's new desk job leaves him feeling \
                         like this may be his last great road trip",
-                        "https://metrouk2.files.wordpress.com/2014/02/ad_127934252.jpg",
+                        "https://metrouk2.files.wordpress.com/2014/02/ad_12793"
+                        "4252.jpg",
                         "https://www.youtube.com/watch?v=B16Bo47KS2g")
 
 weekend_at_bernies = media.Movie("Weekend at Bernies",
                                  "Two friends go to great lengths to hide a \
                                  murder in this tragic tail of bad luck in \
                                  greed based on a true story.",
-                                 "https://image.tmdb.org/t/p/original/h3hjMREZEUPtDkZBiYDzLq0THk0.jpg",
+                                 "https://image.tmdb.org/t/p/original/h3hjMREZ"
+                                 "EUPtDkZBiYDzLq0THk0.jpg",
                                  "https://www.youtube.com/watch?v=YCTgcZ6ImsQ")
 
 weekend_at_bernies_2 = media.Movie("Weekend at Bernies 2: Reloaded",
@@ -54,13 +59,16 @@ weekend_at_bernies_2 = media.Movie("Weekend at Bernies 2: Reloaded",
                                    as this follow up to the Oscar winner \
                                    showcases the trial and aftermath of the \
                                    notorious case.",
-                                   "https://www.movieposter.com/posters/archive/main/16/A70-8099",
-                                   "https://www.youtube.com/watch?v=ZlCADShkhjk")
+                                   "https://www.movieposter.com/posters/archiv"
+                                   "e/main/16/A70-8099",
+                                   "https://www.youtube.com/watch?v=ZlCADShkhj"
+                                   "k")
 '''
 This lists all the movie objects so they can be called with
 fresh_tomatoes.open_movies_page that will then generate our HTML.
 '''
 
-movies = [shining, mad_max, it_follows, guardians, weekend_at_bernies, weekend_at_bernies_2]
+movies = [shining, mad_max, it_follows, guardians, weekend_at_bernies,
+          weekend_at_bernies_2]
 
 fresh_tomatoes.open_movies_page(movies)
